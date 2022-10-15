f564f94a869e879ce297ec0ed696138cac71313b
Hvbert1 <henryjiafu@gmail.com>
2022-10-15 11:54:05 +1100

    change UI colors

--- eas.js
+++ eas.js
@@ -22,7 +22,7 @@ function change() {
     var cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
         cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'yellow';
+        cell.style.backgroundColor = '#6CBFD4';
     });
 });
 }
